ce38cd59c71e1463dbc9003683040bb514f9ec96
Dylan Bickerstaff <dylan@belowaverage.org>
2021-02-13 15:43:27 -0500

    Added acrylic theming.

--- SuperLauncher/Elements/SuperLauncherIcon.xaml.cs
+++ SuperLauncher/Elements/SuperLauncherIcon.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,19 +21,28 @@ namespace SuperLauncher.Elements
     /// </summary>
     public partial class SuperLauncherIcon : UserControl
     {
+        private DoubleAnimation AniFadeIn = new DoubleAnimation()
+        {
+            Duration = new Duration(TimeSpan.FromMilliseconds(100)),
+            To = 0.5
+        };
+        private DoubleAnimation AniFadeOut = new DoubleAnimation()
+        {
+            Duration = new Duration(TimeSpan.FromMilliseconds(100)),
+            To = 1
+        };
         public SuperLauncherIcon()
         {
             InitializeComponent();
         }
-
         private void UserControl_MouseEnter(object sender, MouseEventArgs e)
         {
-            ControlBG.Opacity = 0.5;
+            ControlBG.BeginAnimation(OpacityProperty, AniFadeIn);
         }
 
         private void UserControl_MouseLeave(object sender, MouseEventArgs e)
         {
-            ControlBG.Opacity = 1;
+            ControlBG.BeginAnimation(OpacityProperty, AniFadeOut);
         }
     }
 }

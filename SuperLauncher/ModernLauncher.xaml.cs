21443a93e56a27ba33c4afd9750f0f48701b4982
Dylan Bickerstaff <dylan@belowaverage.org>
2022-03-12 02:17:08 -0500

    Coloring

--- SuperLauncher/ModernLauncher.xaml.cs
+++ SuperLauncher/ModernLauncher.xaml.cs
@@ -26,21 +26,10 @@ namespace SuperLauncher
         {
             InitializeComponent();
         }
-        public void SetBackground()
-        {
-            /*User32.SafeDCHandle screen = User32.GetDC(IntPtr.Zero);
-            User32.SafeDCHandle memory = Gdi32.CreateCompatibleDC(screen);
-            IntPtr bitmap = Gdi32.CreateCompatibleBitmap(screen, (int)SystemParameters.VirtualScreenWidth, (int)SystemParameters.VirtualScreenHeight);
-            Gdi32.SelectObject(memory, bitmap);
-            Gdi32.BitBlt(memory.HWnd, 0, 0, 100, 100, screen.HWnd, 0, 0, 0x00CC0020);
-            BitmapSource bitmaSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
-            Background = new ImageBrush(bitmaSource);*/
-        }
         private void Window_Activated(object sender, EventArgs e)
         {
-            SetBackground();
+           
         }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             Win32Interop.EnableBlur(new WindowInteropHelper(this).Handle, 200, 0);

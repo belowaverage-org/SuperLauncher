503a67894bc0fb79c7f90186fd83c7c08163069d
Dylan Bickerstaff <dylan@belowaverage.org>
2022-04-17 15:40:06 -0400

    .

--- SuperLauncher/ModernLauncher.xaml.cs
+++ SuperLauncher/ModernLauncher.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Interop;
 using PInvoke;
 using System.Windows.Media.Animation;
 using System.Timers;
+using System.Diagnostics;
 //using Image = System.Drawing.Image;
 
 namespace SuperLauncher
@@ -25,32 +26,67 @@ namespace SuperLauncher
     public partial class ModernLauncher : Window
     {
         private WindowInteropHelper WIH;
-        private int y = 0;
-        private int ySpeed = 65;
-        private int screenHeight = (int)SystemParameters.FullPrimaryScreenHeight;
+        private Int32Animation OpenAnimation;
+        private Int32Animation CloseAnimation;
         public ModernLauncher()
         {
             InitializeComponent();
             WIH = new(this);
         }
-        private void Window_Activated(object sender, EventArgs e)
-        {
-            Top = screenHeight;
-            y = (int)Top;
-        }
-        private async Task OpenAnimation()
-        {
-            while (true)
-            {
-                await Task.Delay(TimeSpan.FromMilliseconds(2));
-                User32.MoveWindow(WIH.Handle, (int)Left, y -= (ySpeed -= 4), (int)Width, (int)Height, true);
-                if (y <= 0) break;
-            }
-        }
+        public static readonly DependencyProperty Win32TopProperty = DependencyProperty.Register(
+            "Win32Top",
+            typeof(int),
+            typeof(ModernLauncher),
+            new FrameworkPropertyMetadata(
+                new PropertyChangedCallback(WindowPositionChanged)
+            )
+        );
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            SetPosition();
+            CalculateAnimations();
             Win32Interop.EnableBlur(WIH.Handle, 200, 0);
-            _ = OpenAnimation();
+            _ = Task.Run(() => {
+                Dispatcher.BeginInvoke(async () =>
+                {
+                    while (true)
+                    {
+                        await Task.Delay(600);
+                        BeginAnimation(Win32TopProperty, OpenAnimation);
+                        await Task.Delay(600);
+                        BeginAnimation(Win32TopProperty, CloseAnimation);
+                    }
+                });
+            });
+        }
+        private void SetPosition()
+        {
+            Top = SystemParameters.PrimaryScreenHeight;
+            Left = SystemParameters.PrimaryScreenWidth - Width - 10;
+        }
+        private void CalculateAnimations()
+        {
+            OpenAnimation = new()
+            {
+                Duration = TimeSpan.FromSeconds(0.3),
+                From = (int)SystemParameters.PrimaryScreenHeight,
+                To = (int)(SystemParameters.PrimaryScreenHeight - Height) - 60,
+                EasingFunction = new QuarticEase() { EasingMode = EasingMode.EaseOut }
+            };
+            CloseAnimation = new()
+            {
+                Duration = TimeSpan.FromSeconds(0.3),
+                To = (int)SystemParameters.PrimaryScreenHeight,
+                From = (int)(SystemParameters.PrimaryScreenHeight - Height) - 60,
+                EasingFunction = new QuarticEase() { EasingMode = EasingMode.EaseIn }
+            };
+            Timeline.SetDesiredFrameRate(OpenAnimation, 300);
+            Timeline.SetDesiredFrameRate(CloseAnimation, 300);
+        }
+        public static void WindowPositionChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            ModernLauncher win = (ModernLauncher)obj;
+            User32.MoveWindow(win.WIH.Handle, (int)win.Left, (int)e.NewValue, (int)win.Width, (int)win.Height, false);
         }
     }
 }

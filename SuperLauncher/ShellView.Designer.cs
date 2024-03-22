1b4c1f1ebd4d05dfd9997d07d76cc84a25aa8d84
Dylan Bickerstaff <dylan.bickerstaff@cedarfair.com>
2024-03-22 14:44:19 -0400

    .

--- SuperLauncher/ShellView.Designer.cs
+++ SuperLauncher/ShellView.Designer.cs
@@ -28,45 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtNav = new System.Windows.Forms.ComboBox();
-            this.SuspendLayout();
+            txtNav = new System.Windows.Forms.ComboBox();
+            MiMenu = new System.Windows.Forms.MenuStrip();
+            MIOpenWith = new System.Windows.Forms.ToolStripMenuItem();
+            MiMenu.SuspendLayout();
+            SuspendLayout();
             // 
             // txtNav
             // 
-            this.txtNav.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtNav.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.txtNav.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources;
-            this.txtNav.ItemHeight = 15;
-            this.txtNav.Location = new System.Drawing.Point(129, 6);
-            this.txtNav.MaxDropDownItems = 30;
-            this.txtNav.Name = "txtNav";
-            this.txtNav.Size = new System.Drawing.Size(441, 23);
-            this.txtNav.TabIndex = 3;
+            txtNav.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            txtNav.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            txtNav.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources;
+            txtNav.ItemHeight = 15;
+            txtNav.Location = new System.Drawing.Point(129, 6);
+            txtNav.MaxDropDownItems = 30;
+            txtNav.Name = "txtNav";
+            txtNav.Size = new System.Drawing.Size(441, 23);
+            txtNav.TabIndex = 3;
+            // 
+            // MiMenu
+            // 
+            MiMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { MIOpenWith });
+            MiMenu.Location = new System.Drawing.Point(0, 0);
+            MiMenu.Name = "MiMenu";
+            MiMenu.Size = new System.Drawing.Size(575, 24);
+            MiMenu.TabIndex = 4;
+            MiMenu.Text = "MiMenu";
+            MiMenu.Visible = false;
+            // 
+            // MIOpenWith
+            // 
+            MIOpenWith.Name = "MIOpenWith";
+            MIOpenWith.Size = new System.Drawing.Size(74, 20);
+            MIOpenWith.Text = "Open with";
             // 
             // ShellView
             // 
-            this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(575, 321);
-            this.Controls.Add(this.txtNav);
-            this.DoubleBuffered = true;
-            this.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.KeyPreview = true;
-            this.Name = "ShellView";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Explorer";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ShellView_FormClosing);
-            this.Load += new System.EventHandler(this.ShellView_Load);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ShellView_KeyDown);
-            this.Resize += new System.EventHandler(this.ShellView_Resize);
-            this.ResumeLayout(false);
-
+            AllowDrop = true;
+            AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            BackColor = System.Drawing.Color.WhiteSmoke;
+            ClientSize = new System.Drawing.Size(575, 321);
+            Controls.Add(txtNav);
+            Controls.Add(MiMenu);
+            DoubleBuffered = true;
+            Font = new System.Drawing.Font("Segoe UI Semilight", 9F);
+            KeyPreview = true;
+            MainMenuStrip = MiMenu;
+            Name = "ShellView";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Explorer";
+            FormClosing += ShellView_FormClosing;
+            Load += ShellView_Load;
+            KeyDown += ShellView_KeyDown;
+            Resize += ShellView_Resize;
+            MiMenu.ResumeLayout(false);
+            MiMenu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private System.Windows.Forms.ComboBox txtNav;
+        private System.Windows.Forms.MenuStrip MiMenu;
+        private System.Windows.Forms.ToolStripMenuItem MIOpenWith;
     }
 }
